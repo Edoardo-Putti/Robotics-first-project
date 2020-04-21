78d77ff44848e04d5898dc27c00ca8bad78d3e21
Edoardo-putti <edo.putti@stud.uniroma3.it>
2020-04-21 19:14:14 +0200

    src with last modifications

---
+++ src/lla2enu/src/collector.cpp
@@ -0,0 +1,71 @@
+  #include "ros/ros.h"
+  #include "std_msgs/String.h"
+  #include "geometry_msgs/QuaternionStamped.h"
+
+  #include <message_filters/subscriber.h>
+  #include <message_filters/time_synchronizer.h>
+  #include <message_filters/sync_policies/exact_time.h>
+  #include <message_filters/sync_policies/approximate_time.h>
+
+  //#include "service/AddTwoInts.h"
+
+
+  class collettore
+  {
+
+
+
+   // ros::Subscriber sub1;
+   // ros::Subscriber sub2;
+  //  ros::Publisher pub;
+
+  public:
+    collettore(){
+        sub1.subscribe(n, "enu_front", 1);
+        sub2.subscribe(n, "enu_obs", 1);
+   //typedef message_filters::sync_policies::ExactTime<geometry_msgs::QuaternionStamped, geometry_msgs::QuaternionStamped> MySyncPolicy;
+        sync.reset(new Sync(MySyncPolicy(10), sub1, sub2));
+        sync->registerCallback(boost::bind(&collettore::callback,this, _1, _2));
+
+        }
+
+
+  void callback(const geometry_msgs::QuaternionStampedConstPtr& front_msg, const geometry_msgs::QuaternionStampedConstPtr& obs_msg)
+  {
+    ROS_INFO ("Received two messages: (%f,%f,%f ,%f) and (%f,%f,%f,%f)", front_msg->quaternion.x, front_msg->quaternion.y,front_msg->quaternion.z, front_msg->quaternion.w, obs_msg->quaternion.x, obs_msg->quaternion.y,obs_msg->quaternion.z, obs_msg->quaternion.w);
+
+    if (front_msg->quaternion.w == 0 || obs_msg->quaternion.w == 0){
+
+      ROS_INFO ("banana : GPS Missing ");
+
+    }
+
+    else{
+
+      ROS_INFO ("banana : GPS Funziona ");
+
+    }
+  }
+
+  private:
+    ros::NodeHandle n;
+    message_filters::Subscriber<geometry_msgs::QuaternionStamped> sub1;
+    message_filters::Subscriber<geometry_msgs::QuaternionStamped> sub2;
+    typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::QuaternionStamped, geometry_msgs::QuaternionStamped> MySyncPolicy;
+    typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+    boost::shared_ptr<Sync> sync;
+
+  };
+
+  int main(int argc, char** argv)
+  {
+    ros::init(argc, argv, "collector");
+
+  collettore my_collettore;
+
+    ros::spin();
+
+    return 0;
+  }
+
+
---
+++ src/lla2enu/src/sub.cpp
@@ -0,0 +1,146 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/NavSatFix.h"
+#include <math.h>
+#include "geometry_msgs/QuaternionStamped.h"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+
+class pub_sub
+{
+/*
+message structure of a quaternion 4 float x,y,z,w.
+float w will be the value which will tell us if the GPS work or not.
+Can't use x=0,y=0,z=0 since if i'm in the initial position i will have that
+xd , yd, zd = 0 and so xEast, yNorth, and zUp equal to zero
+*/
+
+ geometry_msgs::QuaternionStamped p;
+
+
+private:
+ros::NodeHandle n;
+ros::Subscriber sub;
+ros::Publisher pub;
+
+
+public:
+  	pub_sub(){
+  	sub =n.subscribe("/swiftnav/front/gps_pose", 1, &pub_sub::callback, this);
+	  pub = n.advertise<geometry_msgs::QuaternionStamped>("/enu_front", 1);
+
+    }
+
+
+  void callback(const sensor_msgs::NavSatFix::ConstPtr& msg){
+    ROS_INFO("Input position: [%f,%f, %f]", msg->latitude, msg->longitude,msg->altitude);
+
+    // fixed values
+
+    double a = 6378137;
+    double b = 6356752.3142;
+    double f = (a - b) / a;
+    double e_sq = f * (2-f);
+    float deg_to_rad = 0.0174533;
+
+    // input data from msg
+    float latitude = msg->latitude;
+    float longitude = msg->longitude;
+    float h = msg->altitude;
+
+    if (latitude==0 && longitude==0 && h==0){
+
+       ROS_INFO("Non ricevo il GPS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+
+     	p.header.stamp = ros::Time::now();
+		  p.header.frame_id = "car";  
+      p.quaternion.x=0;
+      p.quaternion.y=0;
+      p.quaternion.z=0;
+      p.quaternion.w=0;
+      pub.publish(p);
+
+    }
+  else{
+      // fixed position
+      float latitude_init;
+      float longitude_init;
+      float h0;
+
+      n.getParam ("/latitude_init", latitude_init);
+      n.getParam ("/longitude_init", longitude_init);
+      n.getParam ("/h0", h0);
+
+      ROS_INFO("values: %f %f %f",latitude_init,longitude_init,h0);
+
+
+      //lla to ecef
+      float lamb = deg_to_rad*(latitude);
+      float phi = deg_to_rad*(longitude);
+      float s = sin(lamb);
+      float N = a / sqrt(1 - e_sq * s * s);
+
+      float sin_lambda = sin(lamb);
+      float  cos_lambda = cos(lamb);
+      float  sin_phi = sin(phi);
+      float  cos_phi = cos(phi);
+
+      float  x = (h + N) * cos_lambda * cos_phi;
+      float  y = (h + N) * cos_lambda * sin_phi;
+      float  z = (h + (1 - e_sq) * N) * sin_lambda;
+
+      ROS_INFO("ECEF position: [%f,%f, %f]", x, y,z);
+
+
+      // ecef to enu
+
+      lamb = deg_to_rad*(latitude_init);
+      phi = deg_to_rad*(longitude_init);
+      s = sin(lamb);
+      N = a / sqrt(1 - e_sq * s * s);
+
+      sin_lambda = sin(lamb);
+      cos_lambda = cos(lamb);
+      sin_phi = sin(phi);
+      cos_phi = cos(phi);
+
+      float  x0 = (h0 + N) * cos_lambda * cos_phi;
+      float  y0 = (h0 + N) * cos_lambda * sin_phi;
+      float  z0 = (h0 + (1 - e_sq) * N) * sin_lambda;
+
+      float xd = x - x0;
+      float  yd = y - y0;
+      float  zd = z - z0;
+
+      float  xEast = -sin_phi * xd + cos_phi * yd;
+      float  yNorth = -cos_phi * sin_lambda * xd - sin_lambda * sin_phi * yd + cos_lambda * zd;
+      float  zUp = cos_lambda * cos_phi * xd + cos_lambda * sin_phi * yd + sin_lambda * zd;
+
+      ROS_INFO("ENU position: [%f,%f, %f]", xEast, yNorth,zUp);
+
+
+      p.header.stamp = ros::Time::now();
+		  p.header.frame_id = "car";  
+      p.quaternion.x=xEast;
+      p.quaternion.y=yNorth;
+      p.quaternion.z=zUp;
+      p.quaternion.w=1;
+      pub.publish(p);
+
+    }
+  }
+
+};
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "listener_front");
+
+pub_sub my_pub_sub;
+
+
+  	ros::spin();
+
+  return 0;
+}
