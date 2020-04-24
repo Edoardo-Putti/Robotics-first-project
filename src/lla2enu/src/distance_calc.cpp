bba0333e11cb22ef2f45e8bb0a6df54c8a9c6672
Edoardo-Putti <edo.putti@stud.uniroma3.it>
2020-04-24 16:35:16 +0200

    cambiata la distanza

--- src/lla2enu/src/distance_calc.cpp
+++ src/lla2enu/src/distance_calc.cpp
@@ -8,25 +8,16 @@ bool distanza(lla2enu::DistanceCalculator::Request  &req,
     float toRadians = 0.0174533;
     float R = 6371;     // Radius of Earth in Kilometers, R = 6371 
 
-    float lat1=req.x;
-    float long1=req.y;
-    float lat2=req.x_obs;
-    float long2=req.y_obs;
+    float x1=req.x;
+    float y1=req.y;
+    float z1=req.z;
+    float x2=req.x_obs;
+    float y2=req.y_obs;
+    float z2=req.z_obs
 
 ROS_INFO(" Pronto per calcolare Input x: %f y: %f x_obs: %f y_obs: %f",req.x,req.y,req.x_obs,req.y_obs );
 
-    lat1 = toRadians*lat1; 
-    long1 = toRadians*long1; 
-    lat2 = toRadians*lat2; 
-    long2 = toRadians*long2; 
-
-    // Haversine Formula 
-
-    float dlong = long2 - long1; 
-    float dlat = lat2 - lat1; 
-    float ans = pow(sin(dlat / 2), 2)+cos(lat1)*cos(lat2)*pow(sin(dlong / 2), 2); 
-
-    ans = (2 * asin(sqrt(ans)))*R; 
+    float ans = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2) );
 
     res.distance=ans;
 
